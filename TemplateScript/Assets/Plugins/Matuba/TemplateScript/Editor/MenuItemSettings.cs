f3cb7f7643b497af534cd43017efaea045e9ea68
yukiichinose <nkc.yuki.ichinose@gmail.com>
2024-07-14 02:21:18 +0900

    Adapted to changes in path

--- TemplateScript/Assets/Plugins/Matuba/TemplateScript/Editor/MenuItemSettings.cs
+++ TemplateScript/Assets/Plugins/Matuba/TemplateScript/Editor/MenuItemSettings.cs
@@ -7,8 +7,6 @@ namespace Matuba.Editor
     // Created by Matuba on 2024/06/04
     static internal class MenuItemSettings
     {
-        private const string PluginPath = "Assets/Plugins/Matuba/TemplateScript/";
-
         private const string MenuItemRootPath = "Assets/Create/Template C# Script/";
 
         // メニューの優先度、C#Scriptの上に表示されるように設定
@@ -32,7 +30,7 @@ namespace Matuba.Editor
 
         private static void CreateScriptFile(string templateFileName, string newScriptFileName)
         {
-            string path = Path.Combine(PluginPath, $"Templates/{templateFileName}");
+            string path = Path.Combine(GetPluginPath(), $"Templates/{templateFileName}");
 
             if (!File.Exists(path))
             {
@@ -44,6 +42,26 @@ namespace Matuba.Editor
             (
                 path, newScriptFileName
             );
+        }
+
+        // このスクリプトの絶対ファイルパスを取得し、プラグインのパスを返す
+        private static string GetPluginPath()
+        {
+            string path = GetFilePath();
+
+            DirectoryInfo currentDirectory = new(path);
+
+            return currentDirectory.Parent.Parent.ToString();
+        }
+
+        /// <summary>
+        /// 呼び出し元のスクリプトのファイルパス(OS の絶対ファイルパス)を取得します。
+        /// </summary>
+        /// <param name="filePath">自動的に設定されます。</param>
+        /// <returns>ファイルパス</returns>
+        private static string GetFilePath([System.Runtime.CompilerServices.CallerFilePath] string filePath = null)
+        {
+            return filePath;
         }
     }
 }
